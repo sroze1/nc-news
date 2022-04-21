61fde623abffa762f93ddca51655701c6e5d8b56
S Roze <sherozem4@gmail.com>
2022-04-21 19:21:57 +0100

    Topics complete

--- src/Pages/Topics.jsx
+++ src/Pages/Topics.jsx
@@ -18,6 +18,35 @@ const Topics = () => {
   return (
     <div>
       <h1>Topics: {params.topics}</h1>
+      {console.log(params.topics)}
+      {articles
+        .filter((article) => article.topic === params.topics)
+        .map((item) => {
+          {
+            console.log(item, "new new");
+          }
+          return (
+            <div>
+              <header>
+                <h2>{item.title}</h2>
+              </header>
+
+              <div className="body">
+                <p>{item.body}</p>
+                <div>
+                  {" "}
+                  <p>@{item.author}</p>
+                  <Link to={`/api/articles/${item.article_id}/comments`}>
+                    Comments: {item.comment_count}
+                  </Link>
+                  <button id="votes">{item.votes}</button>
+                  <label htmlFor="votes">Click to upvote</label>
+                  <p> {item.created_at}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
